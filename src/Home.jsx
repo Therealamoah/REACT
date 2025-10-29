e83ef02b6531924a7a5b2da9412a5247642a6742
Therealamoah <collinsamoah594@gmail.com>
2025-10-29 15:13:11 +0000

    React Exercise 2

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-red-500'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum eos harum laboriosam tempore quae voluptatibus dignissimos facilis obcaecati? Veritatis error animi porro expedita corrupti ratione cumque veniam omnis aut sunt!</p>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt excepturi quae cum quis nam itaque repellat mollitia delectus porro. Quam, mollitia ex qui numquam exercitationem necessitatibus rem repudiandae reiciendis voluptate.</h1>
+    </div>
+  )
+}
+
+export default Home
